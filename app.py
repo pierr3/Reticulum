d57c7f729133b1e89be4e14d691e9ad341b5045d
Pierre Ferran <pierr3@users.noreply.github.com>
2020-10-17 01:35:32 +0200

    Based work on parser

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, escape
 from config import *
 from arango import ArangoClient
+from lxml import etree
+import urllib3
 app = Flask(__name__)
 
 # Basic definitions
@@ -22,6 +24,19 @@ class InvalidUsage(Exception):
 # DB Setup
 client = ArangoClient(hosts='http://localhost:8529')
 db = client.db(DB_NAME, username=ARRANGO_USER, password=ARRANGO_PASSWORD)
+cases = db.collection('cases')
+graph = db.graph('casesGraph')
+http = urllib3.PoolManager()
+
+# Get a new case
+def get_new_case(celex, level=2):
+    r = http.request('GET', CELLAR_CELEX_BASE_URL + celex + '?language=ENG',  
+                    headers={ 'Accept': 'application/xml;notice=object' })
+    if r.status is not 200:
+        return "" # Need to handle exception here in the future
+    
+    root = etree.fromstring(r.data)
+    
 
 @app.route('/')
 def hello():
@@ -29,17 +44,28 @@ def hello():
 
 @app.route('/api/v1/cases/check')
 def cases_check():
-    if 'celex' in request.args:
-        celex = int(request.args['id'])
-    else:
-        raise InvalidUsage('No CELEX number field provided.')
+    return "Not implemented yet."
 
 @app.route('/api/v1/cases/get')
 def cases_get():
     if 'celex' in request.args:
-        celex = int(request.args['id'])
+        celex = escape(request.args['celex'])
     else:
         raise InvalidUsage('No CELEX number field provided.')
+
+    # First we check if we have that case in database
+    if not cases.has(celex):
+        get_new_case(celex)
+
+    c = cases.get(celex)
+
+    result = graph.traverse(
+        start_vertex = celex,
+        direction='outbound',
+        strategy='breadthfirst'
+    )
+
+    return result
 
 
 
